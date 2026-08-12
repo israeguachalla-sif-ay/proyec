0d84df18de2118afaca13dfa006734783024ce7d
Israel Guachalla Fernandez <israeguachalla-sif@itpm.edu.bo>
2026-08-12 08:28:00 -0600

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,21 @@
+// Estilos para los títulos h1
+document.querySelectorAll('h1').forEach(el => {
+    el.style.fontSize = '30px';
+});
+
+// Estilos para los títulos h2
+document.querySelectorAll('h2').forEach(el => {
+    el.style.fontSize = '25px';
+});
+
+// Estilos para los títulos h3
+document.querySelectorAll('h3').forEach(el => {
+    el.style.fontSize = '20px';
+});
+
+// Estilos para los párrafos
+document.querySelectorAll('p').forEach(el => {
+    el.style.lineHeight = '1.8';
+});
+
+
---
+++ js2.js
@@ -0,0 +1,9 @@
+const parrafos = document.querySelectorAll("p");
+parrafos.forEach(p => {
+    p.style.lineHeight = "1.75";
+});
+
+const encabezados = document.querySelectorAll("h1");
+encabezados.forEach(h1 => {
+    h1.style.fontWeight = "800";
+});
